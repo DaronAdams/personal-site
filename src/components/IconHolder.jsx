a7c0b5a73cb4333a967dbefa3babc53fd60f924a
DaronAdams <daron.cole@icloud.com>
2023-01-30 09:34:16 -0600

    added scroll to top

--- src/components/IconHolder.jsx
+++ src/components/IconHolder.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
 
+
+
+
 const IconHolder = () => {
+  const [effect, setEffect] = useState(false);
+
+  const handleScrollBtn = () => {
+    
+  }
+
+  const scrollToTop = () =>{
+    window.scrollTo({
+      top: 0, 
+      behavior: 'smooth'
+      /* you can also use 'auto' behaviour
+         in place of 'smooth' */
+    });
+  };
+
+
   return (
     <>
     <div class="relative h-32 w-32 ml-4">
@@ -17,7 +36,11 @@ const IconHolder = () => {
     <div class="relative h-32 w-32 ml-50">
         <div class="fixed bottom-0 right-10 h-20 w-20">
           <div>
-            <i><ArrowCircleUpIcon fontSize='large'/></i>
+            <i
+            className=''
+            onClick={scrollToTop}
+            >
+            <ArrowCircleUpIcon fontSize='large'/></i>
           </div>
         </div>
       </div>
